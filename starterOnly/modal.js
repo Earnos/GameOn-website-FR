86e29d1cafef895810d31655fd27d2ea1056ea6e
Earnos <lcyril33@gmail.com>
2023-07-06 09:35:43 +0200

    email regex fixes

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,62 +13,58 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeForm = document.querySelector(".close");
 
-
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach(btn => btn.addEventListener("click", launchModal));
 // clear form after registered and launch modal again
-modalBtn.forEach((btn) => btn.addEventListener("click", () => {
-  const AfterSubmitClearForm = () => {
-    const frm = document.getElementsByName("reserve")[0];
-  console.log(frm);
-  frm.reset();
-  return false 
-}
-AfterSubmitClearForm()
-}));
+modalBtn.forEach(btn =>
+  btn.addEventListener("click", () => {
+    const AfterSubmitClearForm = () => {
+      const frm = document.getElementsByName("reserve")[0];
+      frm.reset();
+      return false;
+    };
+    AfterSubmitClearForm();
+  })
+);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
-};
+}
 
 // close form window
-closeForm.addEventListener('click', e => {
+closeForm.addEventListener("click", e => {
   modalbg.style.display = "none";
 });
 
-
- // RegEx Inputs selection
+// RegEx Inputs selection
 const firstName = document.getElementById("first");
 const lastName = document.getElementById("last");
 const mail = document.getElementById("email");
 const birthDate = document.getElementById("birthdate");
 const quantity = document.getElementById("quantity");
 
-
 // RegEx test function
- const isValidText = (value) => {
-  const re = /^([^0-9]*)$/;
+const isValidText = value => {
+  const re = /^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/;
+  //^([^0-9]*)$/
   return re.test(value);
 };
 
-const isValidAddress = (value) => {
+const isValidAddress = value => {
   const re = /^[0-9]{1,3}(?:(?:[,. ]){1}[-a-zA-Zàâäéèêëïîôöùûüç]+)*$/;
   return re.test(value);
 };
 
-const isValidEmail = (value) => {
-  const re =
-    /^([a-zA-Z0-9]+(([\.\-\_]?[a-zA-Z0-9]+)+)?)\@(([a-zA-Z0-9]+[\.\-\_])+[a-zA-Z]{2,4})$/;
+const isValidEmail = value => {
+  const re = /^([a-zA-Z0-9]+(([\.\-\_]?[a-zA-Z0-9]+)+)?)\@(([a-zA-Z0-9]+[\.\-\_])+[a-zA-Z]{2,})$/;
   return re.test(value);
 };
 
-const isValidQty = (value) => {
-const re = /^(\d{1,4})(\.\d\d)?$/;
-   return re.test(value)
- }
-
-
+const isValidQty = value => {
+  const re = /^(\d{1,4})$/;
+  return re.test(value);
+};
 
 // RegEx validation functions
 
@@ -77,7 +73,7 @@ firstName.addEventListener("input", () => {
     formData[0].dataset.errorVisible = "false";
   } else {
     formData[0].dataset.error = "Saisie du prénom incorrecte";
-    formData[0].dataset.errorVisible = "true";  
+    formData[0].dataset.errorVisible = "true";
   }
 });
 
@@ -100,7 +96,7 @@ mail.addEventListener("input", () => {
 });
 
 quantity.addEventListener("input", () => {
-  if (isValidQty(quantity.value) || quantity.value == "" ) {
+  if (isValidQty(quantity.value) || quantity.value == "") {
     formData[4].dataset.errorVisible = "false";
   } else {
     formData[4].dataset.error = "Entrer une valeur correcte";
@@ -109,80 +105,85 @@ quantity.addEventListener("input", () => {
 });
 
 // submit's form & comfirm button
- const form = document.getElementById('form');
- const submitBtn = document.querySelector(".btn-submit");
- const formBody = document.querySelector(".modal-body");
- const checkboxRequired = document.getElementById("checkbox1");
- const contentForm = document.querySelector(".content");
+const form = document.getElementById("form");
+const submitBtn = document.querySelector(".btn-submit");
+const formBody = document.querySelector(".modal-body");
+const checkboxRequired = document.getElementById("checkbox1");
+const contentForm = document.querySelector(".content");
 
 // form's submit event
-    form.addEventListener("submit", (e) => {e.preventDefault()}, false);
- 
-// creation of confirmation submit's windows  
-    const afterSubmitWindows = () => {
-      form.style.display = "none";
-      formBody.style.height = "750px";
-      // create p html tag for submit confirmation window
-      p = document.createElement("p");
-      p.innerHTML = "Merci pour votre inscription";
-      formBody.append(p);
-      // creation button after submit window
-      confirmCloseBtn = document.createElement("button");
-      confirmCloseBtn.classList.add("confirmSubmitClose");
-      confirmCloseBtn.type = "button";
-      confirmCloseBtn.innerHTML = "Fermer";
-      formBody.append(confirmCloseBtn);
-      // close event after submit window 
-      const closeAfterSubmit = document.querySelector(".confirmSubmitClose");
-      closeAfterSubmit.addEventListener('click', e =>{
-        modalbg.style.display = "none";
-        // re-display the form for the next time
-        form.style.display = "block";
-      })
-    };
+form.addEventListener(
+  "submit",
+  e => {
+    e.preventDefault();
+  },
+  false
+);
 
-    
+// creation of confirmation submit's windows
+const afterSubmitWindows = () => {
+  form.style.display = "none";
+  formBody.style.height = "750px";
+  // create p html tag for submit confirmation window
+  p = document.createElement("p");
+  p.innerHTML = "Merci pour votre inscription";
+  formBody.append(p);
+  // creation button after submit window
+  confirmCloseBtn = document.createElement("button");
+  confirmCloseBtn.classList.add("confirmSubmitClose");
+  confirmCloseBtn.type = "button";
+  confirmCloseBtn.innerHTML = "Fermer";
+  formBody.append(confirmCloseBtn);
+  // close event after submit window
+  const closeAfterSubmit = document.querySelector(".confirmSubmitClose");
+  closeAfterSubmit.addEventListener("click", e => {
+    modalbg.style.display = "none";
+    // re-display the form for the next time
+    form.style.display = "block";
+  });
+};
+
 // verification if all input has a value
-    const checkValues = () => {
-      //const formDataInput = document.querySelectorAll(".formData > input");
-      const formDataInput = document.querySelectorAll(".text-control");
+const checkValues = () => {
+  //const formDataInput = document.querySelectorAll(".formData > input");
+  const formDataInput = document.querySelectorAll(".text-control");
 
-      for (let i = 0; i < formDataInput.length; i++) {
-        if (!formDataInput[i].value) {
-          alert("Veuillez remplir tous les champs"); 
-          return false;
-        } 
-      } 
-      return true;
-    };   
+  for (let i = 0; i < formDataInput.length; i++) {
+    if (!formDataInput[i].value) {
+      alert("Veuillez remplir tous les champs");
+      return false;
+    }
+  }
+  return true;
+};
 
 //  valid  if checkbox is checked
-    const checkBoxUserCondition = () => {
-      // verify if the checkbox is checked
-      if (!checkboxRequired.checked) {
-      alert("veuillez accepter les conditions d'utilisation");
-      return false;
-      }
-    return true;
-  };
+const checkBoxUserCondition = () => {
+  // verify if the checkbox is checked
+  if (!checkboxRequired.checked) {
+    alert("veuillez accepter les conditions d'utilisation");
+    return false;
+  }
+  return true;
+};
 
- 
 // valid if one checkbox'location is checked
-    const checkedLocation = () => {
-      const locationsCheckBox = document.getElementsByName("location");
+const checkedLocation = () => {
+  const locationsCheckBox = document.getElementsByName("location");
 
-      for (let i=0; i < locationsCheckBox.length; i++) {
-        if (locationsCheckBox[i].checked) {
-          return true
-        }
-      }
-      alert("Veuillez sélectionner une localité");
-      return false;    
-     }
- 
-   
+  for (let i = 0; i < locationsCheckBox.length; i++) {
+    if (locationsCheckBox[i].checked) {
+      return true;
+    }
+  }
+  alert("Veuillez sélectionner une localité");
+  return false;
+};
+
 // send submit if form is checked and has values
-    function validate() {    
-      // Verify all the conditions is ok for submit or display an error
-      checkBoxUserCondition() && checkValues() && checkedLocation() ? afterSubmitWindows() : false;     
-    };
+function validate() {
+  // Verify all the conditions is ok for submit or display an error
+  checkBoxUserCondition() && checkValues() && checkedLocation()
+    ? afterSubmitWindows()
+    : false;
+}
